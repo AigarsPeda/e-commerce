8bcb96bf531c36296ce3d268ab787b22903f2de0
Aigars <aigarspeda@gmail.com>
2019-09-19 13:29:28 +0300

    Added Header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import "./App.css";
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/shop" component={ShopPage} />
-      <Route render={() => <h1>Page not Found 404</h1>} />
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+        <Route render={() => <h1>Page not Found 404</h1>} />
+      </Switch>
+    </div>
   );
 }
 
