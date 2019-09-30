dd25574d425320f15782fca506d438915a9b83c2
Aigars <aigarspeda@gmail.com>
2019-09-30 11:12:01 +0300

    Updated cart utils to group items in cart reducer

--- src/components/cart-icon/cart-icon.component.js
+++ src/components/cart-icon/cart-icon.component.js
@@ -4,15 +4,15 @@ import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
 import "./cart-icon.styles.scss";
 
-const CartIcon = state => (
-  <div className="cart-icon" onClick={state.toggleCartHidden}>
+const CartIcon = ({ toggleCartHidden }) => (
+  <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
     <span className="item-count">0</span>
   </div>
 );
 
-const mapDispatchToProps = state => ({
-  toggleCartHidden: () => state(toggleCartHidden())
+const mapDispatchToProps = dispatch => ({
+  toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
 export default connect(
