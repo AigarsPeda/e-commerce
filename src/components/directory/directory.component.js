baf4edd997b7037873e03007bffe2ab04e2f173a
Aigars <aigarspeda@gmail.com>
2019-10-08 14:13:23 +0300

    Added directory reducer

--- src/components/directory/directory.component.js
+++ src/components/directory/directory.component.js
@@ -1,30 +1,28 @@
-import React, { Component } from "react";
+import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { selectDirectorySections } from "../../redux/directory/directory.selectors";
 import MenuItem from "../menu-item/menu-item.component";
-import { SECTIONS_DATA } from "./directory.data";
 import "./directory.styles.scss";
 
-class Directory extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      sections: SECTIONS_DATA
-    };
-  }
-  render() {
-    return (
-      <div className="directory-menu">
-        {this.state.sections.map(section => (
-          <MenuItem
-            key={section.id}
-            title={section.title.toUpperCase()}
-            imageUrl={section.imageUrl}
-            size={section.size}
-            linkUrl={section.linkUrl}
-          />
-        ))}
-      </div>
-    );
-  }
-}
+const Directory = ({ sections }) => {
+  return (
+    <div className="directory-menu">
+      {sections.map(section => (
+        <MenuItem
+          key={section.id}
+          title={section.title.toUpperCase()}
+          imageUrl={section.imageUrl}
+          size={section.size}
+          linkUrl={section.linkUrl}
+        />
+      ))}
+    </div>
+  );
+};
 
-export default Directory;
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections
+});
+
+export default connect(mapStateToProps)(Directory);
