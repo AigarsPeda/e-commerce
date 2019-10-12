0d625fa69fbd090d9417ea96a0967b689f94cedd
Aigars <aigarspeda@gmail.com>
2019-10-12 20:40:03 +0300

    Added stripes payments

--- src/pages/checkout/checkout.component.js
+++ src/pages/checkout/checkout.component.js
@@ -5,6 +5,7 @@ import {
   selectCartItems,
   selectCartTotal
 } from "../../redux/cart/cart.selectors";
+import StripeCheckOutButton from "../../components/stripe-button/stripe-button.component";
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
 import "./checkout.styles.scss";
 
@@ -34,6 +35,12 @@ const CheckoutPage = ({ cartItems, total }) => {
       <div className="total">
         <span>TOTAL: ${total}</span>
       </div>
+      <div className="test-warning">
+        *Please use the following test credit card for payments*
+        <br />
+        4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+      </div>
+      <StripeCheckOutButton price={total} />
     </div>
   );
 };
