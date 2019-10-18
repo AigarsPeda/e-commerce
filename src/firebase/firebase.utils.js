c3c9367d9bf73378962423256a99765cb94834f9
Aigars Peda <aigarspeda@gmail.com>
2019-10-18 09:51:46 +0300

    Started to bringing shop Data to App

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -53,6 +53,19 @@ export const addCollectionAndDocuments = async (
   return await batch.commit();
 };
 
+export const convertCollectionsSnapshotToMap = collection => {
+  const transformedCollection = collection.docs.map(doc => {
+    const { title, items } = doc.data();
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title: title,
+      items: items
+    };
+  });
+  console.log(transformedCollection);
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
